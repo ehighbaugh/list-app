de06cfcbda87ac914579887546ee4b5b4fc2e4d3
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-22 11:54:10 -0400

    set up FireBase and AngularFire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAPgHHXcZN_fslC0WwiirOzehqFpTkTw3E',
+    authDomain: 'list-app-ng5.firebaseapp.com',
+    databaseURL: 'https://list-app-ng5.firebaseio.com',
+    projectId: 'list-app-ng5',
+    storageBucket: 'list-app-ng5.appspot.com',
+    messagingSenderId: '338451651466'
+  }
 };
