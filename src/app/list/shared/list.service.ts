a0af180cb99b87b9bb7fa60eb4400465103ac5cc
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-22 12:19:28 -0400

    updated services (addItem, removeItem, checkOrUncheckItem)

--- src/app/list/shared/list.service.ts
+++ src/app/list/shared/list.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Injectable()
 export class ListService {
+  list: AngularFireList<any>;
+  constructor(private firebasedb: AngularFireDatabase) { }
 
-  constructor() { }
+  getList() {
+    this.list = this.firebasedb.list('items');
+    return this.list;
+  }
 
+  addItem(item: string) {
+    this.list.push({
+      item: item,
+      isChecked: false
+    });
+  }
+
+  checkOrUncheckItem($key: string, flag: boolean) {
+    this.list.update($key, { isChecked: flag });
+  }
+
+  removeItem($key: string) {
+    this.list.remove($key);
+  }
 }
