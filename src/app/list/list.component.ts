927ac80ca8dbcbf76b342e812d27f2a5457fd32a
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-22 12:45:50 -0400

    added function to add items to list and sort unchecked items to top

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from './shared/list.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [ListService]
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  listArray: any[];
+  constructor(private listService: ListService) { }
 
   ngOnInit() {
+    this.listService.getList().snapshotChanges()
+    .subscribe(item => {
+      this.listArray = [];
+      item.forEach(elem => {
+        const x = elem.payload.toJSON();
+        x['$key'] = elem.key;
+        this.listArray.push(x);
+      });
+
+        // sort unchecked items to top of list
+        this.listArray.sort((a, b) => {
+          return a.isChecked - b.isChecked;
+        });
+    });
   }
 
 }
